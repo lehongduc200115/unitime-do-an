e05b15230e296bc5c489291eb860f286ccb8c968
duc le <duc.le4@mservice.com.vn>
2023-02-27 09:10:53 +0700

    [engine] fix room model + room validator

--- backend/src/room/room.validator.ts
+++ backend/src/room/room.validator.ts
@@ -1,6 +1,5 @@
 import { RouteOptionsValidate } from "@hapi/hapi";
 import Joi from "joi";
-import { ClassType } from "../common/enum";
 
 const idParamValidator = Joi.object({
   id: Joi.string().hex().length(24),
@@ -15,16 +14,8 @@ export const createValidator: RouteOptionsValidate = {
     Joi.object({
       id: Joi.string(),
       department: Joi.string(),
-      weeklyTimetable: Joi.array().items(Joi.array().items(Joi.boolean())),
       capacity: Joi.number(),
-      classType: Joi.string()
-        .valid(...Object.values(ClassType))
-        .default(ClassType.LEC),
-      coordinate: Joi.object({
-        zone: Joi.string().valid("TD", "Q10"),
-        block: Joi.string(),
-        level: Joi.number(),
-      }),
+      classType: Joi.string().optional(),
     })
   ),
 };
